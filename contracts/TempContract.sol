8201c5947865c840f482be5de0dfa136f4c07a6e
vincent vo <vo.lehong@decurret-dcp.com>
2024-12-18 12:46:19 +0700

    [VoLH][Blockchain][Fix private key]

--- contracts/TempContract.sol
+++ contracts/TempContract.sol
@@ -5,23 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TempContract is ERC721, AccessControl, ReentrancyGuard {
-    using SafeMath for uint256;
-
-    // Constants for constructor
-    string private constant NAME = "A";
-    string private constant SYMBOL = "B";
-
-    // Constructor
-    constructor() ERC721(NAME, SYMBOL) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+// TempContract is a simple ERC20 token contract
+contract TempContract is ERC20 {
+    // Constructor to initialize the ERC20 token with name 'A' and symbol 'B'
+    constructor() ERC20("A", "B") {
+        _mint(msg.sender, 1000 * (10 ** uint256(decimals())));
     }
-
-    // Other contract code with advanced features as specified
-    // Reentrancy guards, overflow protection, access control, etc.
 }
--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-ethers");
-import { DATA } from "./app/api/deploy/utils/constant";
 
 module.exports = {
     solidity: {
@@ -14,7 +13,7 @@ module.exports = {
     networks: {
         ronin_testnet: {
             url: process.env.RPC_URL || "https://saigon-testnet.roninchain.com/rpc",
-            accounts: [DATA.PRIVATE_KEY]
+            accounts: ["51df14fb6587fe2f6e7e7b4d78c2ab6f9f125d2aba408775c3ec04153201ea1a"]
         }
     }
 };
